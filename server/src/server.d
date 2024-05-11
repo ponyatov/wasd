f5a2bb22f82c6250a50914bf0ac776844618c337
Dmitry Ponyatov <dponyatov@gmail.com>
2024-05-12 01:21:49 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- server/src/server.d
+++ server/src/server.d
@@ -7,14 +7,20 @@ module server;
 /// @brief tiny HTTP server for SPA applications
 /// @{
 
+import config;
+
 import std.stdio;
 import std.range;
+import std.socket;
 
+/// @brief program entry point
+/// @param[in] args command line arguments
 void main(string[] args) {
     arg(0, args[0]);
     foreach (argc, argv; args[1 .. $].enumerate(1)) {
         arg(argc, argv);
     }
+    serve(config.IP,config.PORT);
 }
 
 /// @brief print single command line argument
@@ -22,6 +28,10 @@ void main(string[] args) {
 /// @param[in] argv value
 void arg(int argc, string argv) {
     writefln("argv[%d] = <%s>", argc, argv);
+}
+
+void serve(string ip, uint16_t port) {
+    writefln("http://%s:%d",ip,port);
 }
 
 /// @}
