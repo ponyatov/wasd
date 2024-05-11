d45b507f3350c5d438768383e46442ec99846f5f
Dmitry Ponyatov <dponyatov@gmail.com>
2024-05-12 03:51:19 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- server/src/server.d
+++ server/src/server.d
@@ -11,6 +11,7 @@ import config;
 
 import std.stdio;
 import std.range;
+import std.string;
 import std.socket;
 
 /// @brief program entry point
@@ -20,7 +21,7 @@ void main(string[] args) {
     foreach (argc, argv; args[1 .. $].enumerate(1)) {
         arg(argc, argv);
     }
-    serve(args[0], config.IP, config.PORT);
+    serve(args[0]);
 }
 
 /// @brief print single command line argument
@@ -30,38 +31,48 @@ void arg(int argc, string argv) {
     writefln("argv[%d] = <%s>", argc, argv);
 }
 
-import pegged.grammar;
-
-mixin(grammar(`
-    http:
-        head     < 'GET' [/a-z\.]+ :"HTTP/1.1"
-`));
-
+/// @name static files compiled into binary
+/// @{
 const index_html = cast(immutable ubyte[]) import("index.html");
 const css_css = cast(immutable ubyte[]) import("css.css");
 const logo_png = cast(immutable ubyte[]) import("logo.png");
+/// @}
 
+/// @name MIME types
+/// @{
 const ok = "HTTP/1.1 200 OK\n"c;
 const plain = "Content-Type: text/plain; charset=utf-8\n"c;
 const png = "Content-Type: image/png\n"c;
+/// @}
 
-void serve(string argv0, string ip, ushort port) {
+/// @name network i/o
+
+/// @brief get a parse HTTP request (most dumb way: only first header line)
+/// @param[in] acrtive client socket
+string[] http_request(Socket client) {
+    writef("%s %s\t", client.remoteAddress, client.hostName);
+    char[1024] buffer;
+    auto got = client.receive(buffer);
+    return buffer[0 .. got].idup().splitLines()[0].split(' ');
+}
+
+/// @brief serve HTTP request (single thread, no socket.select)
+/// @param[in] argv0 server binary/executeable file name (for log)
+/// @param[in] ip @ref config.IP
+/// @param[in] port @ref config.PORT
+void serve(string argv0, string ip = config.IP, ushort port = config.PORT) {
     auto listener = new Socket(AddressFamily.INET, SocketType.STREAM);
     writefln("%s @ http://%s:%d (%s)", argv0, ip, port, listener.hostName);
     listener.bind(new InternetAddress(ip, port));
     listener.listen(1);
     while (true) {
-        // recv
+        // http_request
         auto client = listener.accept();
-        writef("%s %s\t", client.remoteAddress, client.hostName);
-        char[1024] buffer;
-        auto got = client.receive(buffer);
-        writeln(buffer[0..got]);
-        // resp
-        auto req = http(buffer[0 .. got].idup())[0].matches;
+        auto req = http_request(client);
         auto method = req[0];
         auto url = req[1];
         writeln(method, ' ', url);
+        // resp
         switch (method) {
         case "GET":
             switch (url) {
