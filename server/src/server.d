637ce96d3799734ce3b3cc8e085cf076665954ca
Dmitry Ponyatov <dponyatov@gmail.com>
2024-05-12 02:47:54 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- server/src/server.d
+++ server/src/server.d
@@ -34,7 +34,7 @@ import pegged.grammar;
 
 mixin(grammar(`
     http:
-        head     < "GET"
+        head     < 'GET' [/a-z\.]+ :"HTTP/1.1"
 `));
 
 void serve(string argv0, string ip, ushort port) {
@@ -42,18 +42,24 @@ void serve(string argv0, string ip, ushort port) {
     writefln("%s @ http://%s:%d (%s)", argv0, ip, port, listener.hostName);
     listener.bind(new InternetAddress(ip, port));
     listener.listen(1);
-    while (true) {
-        auto client = listener.accept();
-        writefln("%s %s", client.remoteAddress, client.hostName);
-        char[1024] buffer;
-        auto got = client.receive(buffer);
-        client.send("HTTP/1.1 200 OK\n"c);
-        client.send("Content-Type: text/plain; charset=utf-8\n"c);
-        client.send("\n"c);
-        client.send(buffer[0 .. got]);
-        client.close();
-        writeln(http(buffer[0 .. got].idup()));
+    // while (true) {
+    // recv
+    auto client = listener.accept();
+    writefln("%s %s", client.remoteAddress, client.hostName);
+    char[1024] buffer;
+    auto got = client.receive(buffer);
+    foreach (i; http(buffer[0 .. got].idup())) {
+        foreach (j; i) {
+            writeln(j);
+        }
     }
+    // resp
+    client.send("HTTP/1.1 200 OK\n"c);
+    client.send("Content-Type: text/plain; charset=utf-8\n"c);
+    client.send("\n"c);
+    client.send(buffer[0 .. got]);
+    client.close();
+    // }
 }
 
 /// @}
